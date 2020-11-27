f5400959254ba6c4632fa6c49403646c683c100f
Fedcomp <aglergen@gmail.com>
2020-11-27 18:22:52 +0300

    Initial commit of X Window setup request

--- xodium/src/protocol.rs
+++ xodium/src/protocol.rs
@@ -1,5 +1,51 @@
-mod framed;
 mod setup_codec;
+mod setup_request;
 
-pub(crate) use self::framed::Framed;
 pub(crate) use self::setup_codec::SetupCodec;
+
+use std::io::{self, Read, Write};
+
+/// Protocol byte order.
+/// X Window protocol allows us to specify connection endianness,
+/// but we always use little endian as we target linux x86 cpu's.
+pub(crate) const BYTE_ORDER: u8 = b'l';
+
+/// X Window System protocol major version
+pub(crate) const PROTOCOL_MAJOR_VERSION: u16 = 11;
+/// X Window System protocol minor version
+pub(crate) const PROTOCOL_MINOR_VERSION: u16 = 0;
+
+#[allow(dead_code)]
+pub(crate) type CARD8 = u8;
+#[allow(dead_code)]
+pub(crate) type CARD16 = u16;
+#[allow(dead_code)]
+pub(crate) type CARD32 = u32;
+#[allow(dead_code)]
+pub(crate) type CARD64 = u64;
+#[allow(dead_code)]
+pub(crate) type INT8 = i8;
+#[allow(dead_code)]
+pub(crate) type INT32 = i32;
+#[allow(dead_code)]
+pub(crate) type INT16 = i16;
+#[allow(dead_code)]
+pub(crate) type INT64 = i64;
+#[allow(dead_code)]
+pub(crate) type BYTE = u8;
+#[allow(dead_code)]
+pub(crate) type BOOL = bool;
+
+/// General crate serialization trait.
+pub trait Serialize {
+    fn serialize<W: Write>(&self, writer: W) -> io::Result<()>;
+}
+
+/// General crate deserialization trait.
+pub trait Deserialize: Sized {
+    fn deserialize<R: Read>(reader: R) -> io::Result<Option<Self>>;
+}
+
+pub fn pad(e: usize) -> usize {
+    (4 - (e % 4)) % 4
+}
---
+++ xodium/src/protocol/setup_request.rs
@@ -0,0 +1,99 @@
+use super::{pad, Serialize, BYTE_ORDER, PROTOCOL_MAJOR_VERSION, PROTOCOL_MINOR_VERSION};
+use std::convert::TryFrom;
+use std::io::{self, Write};
+use std::num::TryFromIntError;
+
+/// Request a connection to X server
+pub(crate) struct SetupRequest {
+    auth_protocol_name: Vec<u8>,
+    auth_protocol_data: Vec<u8>,
+}
+
+impl SetupRequest {
+    pub fn new(
+        auth_protocol_name: Vec<u8>,
+        auth_protocol_data: Vec<u8>,
+    ) -> Result<SetupRequest, TryFromIntError> {
+        // Ensure protocol data and name len() fit u16 required by protocol.
+        u16::try_from(auth_protocol_name.len())?;
+        u16::try_from(auth_protocol_data.len())?;
+
+        Ok(SetupRequest {
+            auth_protocol_name,
+            auth_protocol_data,
+        })
+    }
+}
+
+// 1                       byte-order
+//       #x42     MSB first
+//       #x6C     LSB first
+// 1                       unused
+// 2     CARD16            protocol-major-version
+// 2     CARD16            protocol-minor-version
+// 2     n                 length of authorization-protocol-name
+// 2     d                 length of authorization-protocol-data
+// 2                       unused
+// n     STRING8           authorization-protocol-name
+// p                       unused, p=pad(n)
+// d     STRING8           authorization-protocol-data
+// q                       unused, q=pad(d)
+impl Serialize for SetupRequest {
+    fn serialize<W: Write>(&self, mut writer: W) -> io::Result<()> {
+        writer.write_all(&[BYTE_ORDER])?;
+        writer.write_all(&[0])?; // pad
+        writer.write_all(&PROTOCOL_MAJOR_VERSION.to_le_bytes())?;
+        writer.write_all(&PROTOCOL_MINOR_VERSION.to_le_bytes())?;
+        // We ensure protocol name and data are u16 in new(),
+        // and never allow build the struct any other way.
+        writer.write_all(&(self.auth_protocol_name.len() as u16).to_le_bytes())?;
+        writer.write_all(&(self.auth_protocol_data.len() as u16).to_le_bytes())?;
+        writer.write_all(&[0, 0])?; // pad
+
+        writer.write_all(&self.auth_protocol_name[..])?;
+        for _ in 0..pad(self.auth_protocol_name.len()) {
+            writer.write_all(&[0])?;
+        }
+
+        writer.write_all(&self.auth_protocol_data[..])?;
+        for _ in 0..pad(self.auth_protocol_data.len()) {
+            writer.write_all(&[0])?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SetupRequest;
+    use crate::protocol::Serialize;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_serialize_empty() {
+        const EXPECTED_AUTHORIZATION_BUF: &[u8] = b"l\0\x0b\0\0\0\0\0\0\0\0\0";
+        let mut write_buf = vec![];
+
+        SetupRequest::new(vec![], vec![])
+            .expect("Empty vecs always pass")
+            .serialize(&mut Cursor::new(&mut write_buf))
+            .unwrap();
+
+        assert_eq!(write_buf, EXPECTED_AUTHORIZATION_BUF);
+    }
+
+    #[test]
+    fn test_serialize_auth_data() {
+        const EXPECTED_AUTHORIZATION_BUF: &[u8] =
+            b"l\0\x0b\0\0\0\t\0\t\0\0\0auth_name\0\0\0auth_data\0\0\0";
+        let mut write_buf = vec![];
+
+        SetupRequest::new(b"auth_name".to_vec(), b"auth_data".to_vec())
+            .expect("Specified values always pass")
+            .serialize(&mut Cursor::new(&mut write_buf))
+            .unwrap();
+
+        assert_eq!(write_buf, EXPECTED_AUTHORIZATION_BUF);
+    }
+}
